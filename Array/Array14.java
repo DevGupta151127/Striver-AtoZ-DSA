8d9c68fcc9f192992df7ba056db3e48fdbe1a8c2
DevGupta151127 <155311990+DevGupta151127@users.noreply.github.com>
2024-07-16 10:22:10 +0530

    Longest subarray with sum K (Positives + Negatives)

---
+++ Array/Array14.java
@@ -0,0 +1,30 @@
+package Array;
+
+import java.util.Scanner;
+
+public class Array14 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the size of array: ");
+        int n=sc.nextInt();
+        int[] array=new int[n];
+        System.out.print("Enter the elements of array: ");
+        for(int i=0;i<n;i++){
+            array[i]=sc.nextInt();
+        }
+        System.out.print("Enter the sum for which you need longest subarray: ");
+        int k=sc.nextInt();
+        int len=0;
+        for(int i=0;i<n;i++){
+            int sum=0;
+            for(int j=i;j<n;j++){
+                sum+=array[j];
+                if(sum==k){
+                    len=Math.max(len, j-i+1);
+                }
+            }
+        }
+        System.out.println("The longest subarray with given sum is: "+len);
+        sc.close();
+    }
+}
